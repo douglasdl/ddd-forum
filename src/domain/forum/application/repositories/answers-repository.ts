a2890b3c9f705115c955eb42837559cd6058e27b
douglasdl <douglas_san@hotmail.com>
2025-02-14 13:13:35 +0900

    feat: add Fetch Question Answers

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '../../enterprise/entitites/answer'
 
 export interface AnswersRepository {
   findById(id: string): Promise<Answer | null>
+  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<Answer[]>
   save(answer: Answer): Promise<void>
   create(answer: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
