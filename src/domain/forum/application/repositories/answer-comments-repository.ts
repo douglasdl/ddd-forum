64b386e4a2646b23ccb668baf9f613b967b3dbee
douglasdl <douglas_san@hotmail.com>
2025-02-14 15:54:07 +0900

    feat: add List Answer Comments usecase

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswerComment } from "../../enterprise/entitites/answer-comment"
 
 export interface AnswerCommentsRepository {
   findById(id: string): Promise<AnswerComment | null>
+  findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
   create(answerComment: AnswerComment): Promise<void>
   delete(answerComment: AnswerComment): Promise<void>
 }
