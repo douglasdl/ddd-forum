d510a7e36ffba65b3cd4db9d0400883bd5bacc2a
douglasdl <douglas_san@hotmail.com>
2025-02-14 14:50:06 +0900

    feat: add deleteCommentFromQuestion usecase

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -4,7 +4,22 @@ import { QuestionComment } from "@/domain/forum/enterprise/entitites/question-co
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    const questionComment = this.items.find(item => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   async create(questioncomment: QuestionComment) {
     this.items.push(questioncomment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(item => item.id === questionComment.id)
+    this.items.splice(itemIndex, 1)
   }
 }
