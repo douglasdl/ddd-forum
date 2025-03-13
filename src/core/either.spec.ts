3c972f40653ab65db518655064261fc194fdad9f
douglasdl <douglas_san@hotmail.com>
2025-03-13 13:06:13 +0900

    feat: add tests for the Either class

---
+++ src/core/either.spec.ts
@@ -0,0 +1,26 @@
+import { type Either, left, right } from "./either"
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if(shouldSuccess) {
+    return right(10)
+  } 
+  return left('error')
+}
+
+test('Success result', () => {
+  const result = doSomething(true)
+
+  if(result.isRight()) {
+    console.log(result.value)
+  }
+
+  expect(result.isRight()).toBe(true)
+  expect(result.isLeft()).toBe(false)
+})
+
+test('Error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft).toBe(true)
+  expect(result.isRight).toBe(false)
+})
