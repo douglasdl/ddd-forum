791016d0c7b9f2eef9b24a3e6bf8e5ce6d37f580
douglasdl <douglas_san@hotmail.com>
2025-04-09 13:46:00 +0900

    feat: add read notifcation useCase

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,7 +1,7 @@
 import { type Either, left, right } from '@/core/either'
 import type { Question } from '../../enterprise/entitites/question'
 import type { QuestionsRepository } from '../repositories/questions-repository'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
